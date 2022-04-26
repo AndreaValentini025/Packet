c65e644aae8424392da4f1d7de323c9346626f3e
AlleArto <artonialessandro.bus@gmail.com>
2022-04-26 10:38:51 +0200

    login

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="polls-AndreVale025",
-    version="0.1.66",
+    version="0.1.67",
     author="Example Author",
     author_email="author@example.com",
     description="A small example package",
