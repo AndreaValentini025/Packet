c44aedb32eab43b2ac5294866753c585377de8ff
andre <andreavalentini2025@gmail.com>
2022-04-29 19:25:50 +0200

    login and logout fixed

--- application/AttivitaProgettuale/views.py
+++ application/AttivitaProgettuale/views.py
@@ -69,7 +69,7 @@ def update_state(request, richiesta_id):
         richiesta = get_object_or_404(Richiesta, pk=richiesta_id)
         print(richiesta.updated_at + datetime.timedelta(minutes=1))
         print(timezone.now())
-        print(((richiesta.updated_at + datetime.timedelta(minutes=1)) > timezone.now()))
+        print(((richiesta.updated_at + datetime.timedelta(minutes=1)) < timezone.now()))
         if (richiesta.updated_at + datetime.timedelta(minutes=1)) > timezone.now():
             richiesta.stato += 1
             richiesta.save()
