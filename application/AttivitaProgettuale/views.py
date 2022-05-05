49269aa180c29886f7400e82adf35ca795336d32
AlleArto <artonialessandro.bus@gmail.com>
2022-05-05 17:32:55 +0200

    richiesta

--- application/AttivitaProgettuale/views.py
+++ application/AttivitaProgettuale/views.py
@@ -1,7 +1,6 @@
 import datetime
 from django.utils import timezone
 
-from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
