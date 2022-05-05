df9765dfc74e6eeb5bdc297a35e182c8f5db4c0b
andre <andreavalentini2025@gmail.com>
2022-05-05 19:07:24 +0200

    gestione gruppi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="AttivitaProgettuale-VAProject",
-    version="0.2.91",
+    version="0.2.92",
     author="Example Author",
     author_email="author@example.com",
     description="A small example package",
