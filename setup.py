6103bf95b04dbf429c79f08d97729e68f0c76965
andre <andreavalentini2025@gmail.com>
2022-06-23 13:40:43 +0200

    prova pdf

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="AttivitaProgettuale-VAProject",
-    version="0.3.40",
+    version="0.3.41",
     author="Example Author",
     author_email="author@example.com",
     description="A small example package",
